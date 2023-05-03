9f65b0f547834f10d941655270de3a66513b3515
leonel caroc <leonelcaroc25@gmail.com>
2023-05-03 17:06:46 +0800

    edit Card & Slider Component

--- client/src/components/Slider/Slider.jsx
+++ client/src/components/Slider/Slider.jsx
@@ -1,28 +1,40 @@
-import React from "react";
+import { useState } from "react";
 import "./Slider.scss";
 import EastIcon from "@mui/icons-material/East";
 import WestIcon from "@mui/icons-material/West";
 import "./Slider.scss";
 
 const Slider = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
   const data = [
+    "https://images.pexels.com/photos/432059/pexels-photo-432059.jpeg?auto=compress&cs=tinysrgb&w=1600",
     "https://images.pexels.com/photos/1496647/pexels-photo-1496647.jpeg?auto=compress&cs=tinysrgb&w=1600",
-    "https://images.pexels.com/photos/963696/pexels-photo-963696.jpeg?auto=compress&cs=tinysrgb&w=1600",
-    "https://images.pexels.com/photos/247114/pexels-photo-247114.jpeg?auto=compress&cs=tinysrgb&w=600",
+    "https://images.unsplash.com/photo-1483985988355-763728e1935b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000",
   ];
+
+  const prevSlide = () => {
+    setCurrentSlide(currentSlide === 0 ? 2 : (prev) => prev - 1);
+  };
+
+  const nextSlide = () => {
+    setCurrentSlide(currentSlide === 2 ? 0 : (prev) => prev + 1);
+  };
 
   return (
     <div className="slider">
-      <div className="container">
+      <div
+        className="container"
+        style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
+      >
         <img src={data[0]} alt="" />
         <img src={data[1]} alt="" />
         <img src={data[2]} alt="" />
       </div>
       <div className="icons">
-        <div className="icon">
+        <div className="icon" onClick={prevSlide}>
           <WestIcon />
         </div>
-        <div className="icon">
+        <div className="icon" onClick={nextSlide}>
           <EastIcon />
         </div>
       </div>
