713f21d88bffbc475d2730b4beba114dd30a4e36
leonel caroc <leonelcaroc25@gmail.com>
2023-05-03 17:07:04 +0800

    update Featured Product & Home page

---
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import Card from "../Card/Card";
+import "./FeaturedProducts.scss";
+const FeaturedProducts = ({ type }) => {
+  const data = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/2043590/pexels-photo-2043590.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      img2: "https://images.pexels.com/photos/1390602/pexels-photo-1390602.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      title: "Long Sleeve",
+      isNew: true,
+      oldPrice: 19,
+      price: 12,
+    },
+    {
+      id: 2,
+      img: "https://images.pexels.com/photos/1040173/pexels-photo-1040173.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      img2: "https://images.pexels.com/photos/2905238/pexels-photo-2905238.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      title: "Bag",
+      isNew: true,
+      oldPrice: 19,
+      price: 12,
+    },
+    {
+      id: 3,
+      img: "https://images.pexels.com/photos/3317434/pexels-photo-3317434.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      img2: "https://images.pexels.com/photos/1079783/pexels-photo-1079783.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      title: "Hat",
+      isNew: true,
+      oldPrice: 19,
+      price: 12,
+    },
+    {
+      id: 4,
+      img: "https://images.pexels.com/photos/2772535/pexels-photo-2772535.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      img2: "https://images.pexels.com/photos/1484771/pexels-photo-1484771.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      title: "Jacket",
+      isNew: true,
+      oldPrice: 19,
+      price: 12,
+    },
+  ];
+
+  return (
+    <div className="featuredProducts">
+      <div className="top">
+        <h1>{type} products</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+          libero unde cum vel saepe, eaque mollitia dicta, officia, laudantium
+          iusto iste a id beatae aliquam eligendi at sed provident atque. Lorem
+          ipsum dolor sit, amet consectetur adipisicing elit. Rerum deleniti
+        </p>
+      </div>
+      <div className="bottom">
+        {data.map((item) => {
+          return <Card item={item} key={item.id} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedProducts;
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Slider from "../../components/Slider/Slider";
+import FeaturedProducts from "../../components/FeaturedProducts/FeaturedProducts";
 import "./Home.scss";
 
 const Home = () => {
   return (
     <div className="home">
       <Slider />
+      <FeaturedProducts type="featured" />
+      <FeaturedProducts type="trending" />
     </div>
   );
 };
