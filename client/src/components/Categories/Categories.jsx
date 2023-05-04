6867591732db623e7e64f76ad6a5a8d171559ba2
leonel caroc <leonelcaroc25@gmail.com>
2023-05-04 12:50:12 +0800

    created Categories component

---
+++ client/src/components/Categories/Categories.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Categories = () => {
+  return (
+    <div className="categories">
+      <div className="col">
+        <div className="row">r1</div>
+        <div className="row">r2</div>
+      </div>
+      <div className="col">
+        <div className="row">r3</div>
+      </div>
+      <div className="col col-l">
+        <div className="row">
+          <div className="col">
+            <div className="row">r4</div>
+          </div>
+          <div className="col">
+            <div className="row">r5</div>
+          </div>
+        </div>
+        <div className="row">r6</div>
+      </div>
+    </div>
+  );
+};
+
+export default Categories;
