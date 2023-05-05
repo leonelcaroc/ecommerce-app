0ab44f0541cd55019850021956074151eefd28ee
leonel caroc <leonelcaroc25@gmail.com>
2023-05-05 23:30:04 +0800

    update Components

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./Card.scss";
 import { Link } from "react-router-dom";
 
--- client/src/components/FeaturedProducts/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Card from "../Card/Card";
 import "./FeaturedProducts.scss";
 const FeaturedProducts = ({ type }) => {
